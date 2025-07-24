f696dc5ac50c80e2d6af866e5e97a03f7a4e2e84
rdwr-SomashekarBM <SomashekarBM@radware.com>
2025-07-24 14:37:58 +0530

    added some code

--- main.go
+++ main.go
@@ -1,21 +1,94 @@
 package main
 
-import (
-  "fmt"
-)
+//type Person struct {
+//	getName func() string
+//	getAge  func() string
+//}
+//
+//type PersonBuilder struct {
+//	name string
+//	age  string
+//}
+//
+//func (p PersonBuilder) getName() string {
+//	return p.name
+//}
+//
+//func (p PersonBuilder) getAge() string {
+//	return p.age
+//}
+//
+//func PrintDetails(p Person) {
+//	fmt.Printf("Name: %s, Age: %s\n", p.getName(), p.getAge())
+//}
+//
+//func main() {
+//	builder := PersonBuilder{name: "Soma", age: "22"}
+//	p := Person{
+//		getName: builder.getName,
+//		getAge:  builder.getAge,
+//	}
+//	PrintDetails(p)
+//}
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+//func toCheckDefer(name string) string {
+//	defer fmt.Printf("defer: %s\n", name)
+//	fmt.Printf("func: %s\n", name)
+//	return name
+//}
+
+type Person struct {
+	name    string
+	age     int
+	ssn     int
+	ph_no   int
+	address string
+}
+
+func (person Person) getDetails() Person {
+	return person
+}
+
+func getName(person Person) string {
+	return person.name
+}
+
+func getAge(person Person) int {
+	return person.age
+}
+
+func getSsn(person Person) int {
+	return person.ssn
+}
+
+func getAddress(person Person) string {
+	return person.address
+}
+
+func getPhNo(person Person) int {
+	return person.ph_no
+}
+
+func setName(person *Person, name string) {
+	person.name = name
+}
+
+func setAge(person *Person, age int) {
+	person.age = age
+}
+
+func setSsn(person *Person, ssn int) {
+	person.ssn = ssn
+}
+
+func setAddress(person *Person, address string) {
+	person.address = address
+}
+
+func setPhNo(person *Person, ph_no int) {
+	person.ph_no = ph_no
+}
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Printf("Hello and welcome, %s!\n", s)
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
 }
