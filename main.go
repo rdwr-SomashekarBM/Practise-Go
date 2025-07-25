267a18373c61c7deb484eff24fd9705fade15a23
rdwr-SomashekarBM <SomashekarBM@radware.com>
2025-07-25 10:40:28 +0530

    Tried out Gin and one middleware

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"errors"
+	//"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 //type Person struct {
 //	getName func() string
 //	getAge  func() string
@@ -37,58 +44,115 @@ package main
 //	return name
 //}
 
-type Person struct {
-	name    string
-	age     int
-	ssn     int
-	ph_no   int
-	address string
+//type Person struct {
+//	name    string
+//	age     int
+//	ssn     int
+//	ph_no   int
+//	address string
+//}
+//
+//func (person Person) getDetails() Person {
+//	return person
+//}
+//
+//func getName(person Person) string {
+//	return person.name
+//}
+//
+//func getAge(person Person) int {
+//	return person.age
+//}
+//
+//func getSsn(person Person) int {
+//	return person.ssn
+//}
+//
+//func getAddress(person Person) string {
+//	return person.address
+//}
+//
+//func getPhNo(person Person) int {
+//	return person.ph_no
+//}
+//
+//func setName(person *Person, name string) {
+//	person.name = name
+//}
+//
+//func setAge(person *Person, age int) {
+//	person.age = age
+//}
+//
+//func setSsn(person *Person, ssn int) {
+//	person.ssn = ssn
+//}
+//
+//func setAddress(person *Person, address string) {
+//	person.address = address
+//}
+//
+//func setPhNo(person *Person, ph_no int) {
+//	person.ph_no = ph_no
+//}
+
+// Gin Practise:
+
+// middleware for error handling in Gin
+
+func ErrorHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next() // Step1: Process the request first.
+
+		// Step2: Check if any errors were added to the context
+		if len(c.Errors) > 0 {
+			// Step3: Use the last error
+			err := c.Errors.Last().Err
+
+			// Step4: Respond with a generic error message
+			c.JSON(http.StatusInternalServerError, map[string]any{
+				"success": false,
+				"message": err.Error(),
+			})
+		}
+
+		// Any other steps if no errors are found
+	}
 }
 
-func (person Person) getDetails() Person {
-	return person
-}
+func CreateServer() {
+	router := gin.Default()
+	router.Use(ErrorHandler())
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
 
-func getName(person Person) string {
-	return person.name
-}
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
 
-func getAge(person Person) int {
-	return person.age
-}
+	router.GET("/error", func(c *gin.Context) {
+		somethingWentWrong := true
 
-func getSsn(person Person) int {
-	return person.ssn
-}
+		if somethingWentWrong {
+			c.Error(errors.New("something went wrong"))
+			return
+		}
 
-func getAddress(person Person) string {
-	return person.address
-}
+		c.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"message": "Everything is fine!",
+		})
+	})
 
-func getPhNo(person Person) int {
-	return person.ph_no
-}
+	router.Run(":8080")
 
-func setName(person *Person, name string) {
-	person.name = name
-}
-
-func setAge(person *Person, age int) {
-	person.age = age
-}
-
-func setSsn(person *Person, ssn int) {
-	person.ssn = ssn
-}
-
-func setAddress(person *Person, address string) {
-	person.address = address
-}
-
-func setPhNo(person *Person, ph_no int) {
-	person.ph_no = ph_no
 }
 
 func main() {
-
+	CreateServer()
 }
